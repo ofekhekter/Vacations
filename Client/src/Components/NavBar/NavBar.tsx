ce8bb078e9e91e5a3158606b881a9b22c8ab97de
Ofek <ofekh1989@gmail.com>
2024-05-26 16:32:12 +0300

    checkbox

--- Client/src/Components/NavBar/NavBar.tsx
+++ Client/src/Components/NavBar/NavBar.tsx
@@ -1,9 +1,11 @@
+import * as React from 'react';
 import { AppBar, Box, Button, Toolbar, Typography } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { login } from "../../features/loginSlice";
 import { emailAddress } from "../../features/emailSlice";
 import { userRole } from "../../features/adminSlice";
+import Checkbox from '@mui/material/Checkbox';
 import "./navbar.css";
 
 export const Navbar = () => {
@@ -12,6 +14,19 @@ export const Navbar = () => {
     const userEmail = useSelector((state: any) => state.emailAddress.text);
     const isAdmin = useSelector((state: any) => state.userRole.isAdmin);
     const dispatch = useDispatch();
+    const [checkedFollowings, setCheckedFollowings] = React.useState(false);
+    const [checkedUpcoming, setCheckedUpcoming] = React.useState(false);
+    const [checkedCurrently, setCheckedCurrently] = React.useState(false);
+
+    const handleChangeFollowings = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setCheckedFollowings(event.target.checked);
+    };
+    const handleChangeUpcoming = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setCheckedUpcoming(event.target.checked);
+    };
+    const handleChangeCurrently = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setCheckedCurrently(event.target.checked);
+    };
 
     const handleSigninClicked = () => {
         if (loginState === 'Login') {
@@ -25,14 +40,6 @@ export const Navbar = () => {
         }
     }
 
-    const handleHomeClicked = () => {
-        if (loginState === 'Login') {
-            navigate('/signin');
-        } else {
-            navigate('/userpage');
-        }
-    }
-
     const handleCardClicked = () => {
         navigate('/userpage');
     }
@@ -43,17 +50,38 @@ export const Navbar = () => {
 
     return (
         <AppBar className="appBar" position="static" style={{ backgroundColor: "#153448" }}>
-            <Toolbar sx={{ display: 'flex', justifyContent: "space-between"}}>
-                <Typography onClick={handleHomeClicked} variant="h6" component="div">
-                    Home
-                </Typography>
-                <Box sx={{display: "flex"}}>
+            <Toolbar sx={{ display: 'flex', justifyContent: "space-between" }}>
+                <Box sx={{ display: "flex" }}>
                     <Typography variant="h4" sx={{}} component="div">
                         Vacations
                     </Typography>
+                    <Box sx={{ display: "flex", flexDirection: "column", marginLeft: "15px" }}>
+                        Followings
+                        <Checkbox
+                            checked={checkedFollowings}
+                            onChange={handleChangeFollowings}
+                            inputProps={{ 'aria-label': 'controlled' }}
+                        />
+                    </Box>
+                    <Box sx={{ display: "flex", flexDirection: "column", marginLeft: "15px" }}>
+                        Upcoming
+                        <Checkbox
+                            checked={checkedUpcoming}
+                            onChange={handleChangeUpcoming}
+                            inputProps={{ 'aria-label': 'controlled' }}
+                        />
+                    </Box>
+                    <Box sx={{ display: "flex", flexDirection: "column", marginLeft: "15px" }}>
+                        Currently
+                        <Checkbox
+                            checked={checkedCurrently}
+                            onChange={handleChangeCurrently}
+                            inputProps={{ 'aria-label': 'controlled' }}
+                        />
+                    </Box>
                 </Box>
                 <Box sx={{ display: 'flex', alignItems: "center" }}>
-                    <Typography variant="subtitle2" onClick={handleCardClicked} sx={{marginRight: "6px"}}>
+                    <Typography variant="subtitle2" onClick={handleCardClicked} sx={{ marginRight: "6px" }}>
                         {userEmail}
                     </Typography>
                     {isAdmin ? <Button onClick={handleVacationClicked} sx={{ color: "#B0EBB4", border: "1px solid white", marginRight: "6px" }}>Add Vacation</Button> : <span></span>}
